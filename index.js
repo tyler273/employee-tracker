e00bc046241c14f3563c51bffac2d35986dee090
Tyler <t.woods95@aol.com>
2023-06-10 18:54:23 -0400

    Added more inquirer prompts

--- index.js
+++ index.js
@@ -4,8 +4,34 @@ const questionsMainMenu = [
       type: 'list',
       message: 'What would you like to do?',
       name: 'options',
-      choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department']
+      choices: ['View All Employees', 'Add Employee', 'Update Employee Role', 'View All Roles', 'Add Role', 'View All Departments', 'Add Department', 'Exit']
     },
+];
+const questionsAddDepartment = [
+    {
+        type: 'input',
+        message: "What is the name of the department?",
+        name: 'departmentName'
+    }
+];
+
+const questionsAddRole = [
+    {
+        type: 'input',
+        message: "What is the name of the role?",
+        name: 'roleName'
+    },
+    {
+        type: 'input',
+        message: "What is the salary of the role?",
+        name: 'salary'
+    },
+    {
+        type: 'list',
+        message: "Which department does the role belong to?",
+        name: 'departmentList',
+        choices: ['Engineering', 'Finance', 'Legal', 'Sales', 'Service']
+    }
 ];
 
 // TODO: Create a function to initialize app
@@ -16,17 +42,64 @@ function init() {
 // Function call to initialize app
 init();
 
-function processMainMenu(data){
+async function processMainMenu(data){
     switch(data.options){
         case "View All Employees":
             viewAllEmployees();
             break;
         case "Add Employee":
-            console.log("We are going to add an employee!")
+            addEmployee();
+            break;
+        case "Update Employee Role":
+            updateEmployeeRole();
+            break;
+        case "View All Roles":
+            viewAllRoles();
+            break;
+        case "Add Role":
+            await addRole();
+            break;
+        case "View All Departments":
+            viewAllDepartments();
+            break;
+        case "Add Department":
+            await addDepartment();
+            break;
+        case "Exit":
+            exitApp();
             break;
     }
+    init();
 };
 
 function viewAllEmployees(){
     console.log("We will view all employees here!")
+}
+
+function addEmployee(){
+    console.log("We will add an employee!")
+}
+
+function updateEmployeeRole(){
+    console.log("We will update employee role!")
+}
+
+function viewAllRoles(){
+    console.log("We will view all roles")
+}
+
+async function addRole(){
+   await inquirer.prompt(questionsAddRole).then((data) => console.log(data));
+}
+
+function viewAllDepartments(){
+    console.log("We will view all departments!")
+}
+
+async function addDepartment(){
+   await inquirer.prompt(questionsAddDepartment).then((data) => console.log(data.departmentName));
+}
+
+function exitApp(){
+    process.exit()
 }
